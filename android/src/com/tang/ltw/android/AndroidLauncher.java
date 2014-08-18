8a15bcd8373183f352ce5b9cc431bb33a810f350
Alex Tang <Alex@d209-087-58-025.wireless.sfu.ca>
2014-08-17 18:17:43 -0700

    initial commit

---
+++ android/src/com/tang/ltw/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.tang.ltw.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.tang.ltw.LTW;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LTW(), config);
+	}
+}
